6c2e71b25985cd91d4616f92454f4f3cdaa74280
thurXNZ <arthur.mello@aluno.ifsp.edu.br>
2025-02-18 16:55:11 -0300

    :construction: Iniciando o projeto :barber:

---
+++ js/etapas.js
@@ -0,0 +1,102 @@
+let etapas = [
+	{
+		titulo: 'VEREADOR',
+		numeros: 5,
+		candidatos: [
+			{
+				numero: '77111',
+				nome: 'Optimus Prime',
+				partido: 'PA',
+				foto: [
+					{ url: '77111.png', legenda: 'Vereador' }
+				]
+			},
+			{
+				numero: '84001',
+				nome: 'Mestre Yoda',
+				partido: 'PRE',
+				foto: [
+					{ url: '84001.png', legenda: 'Vereador' }
+				]
+			},
+			{
+				numero: '88001',
+				nome: 'Neil Matriz',
+				partido: 'PJM',
+				foto: [
+					{ url: '88001.png', legenda: 'Vereador' }
+				]
+			},
+			{
+				numero: '98111',
+				nome: 'Hellboy',
+				partido: 'PMI',
+				foto: [
+					{ url: '98111.png', legenda: 'Vereador' }
+				]
+			},
+			{
+				numero: '95001',
+				nome: 'Juiz Dredd',
+				partido: 'PLJ',
+				foto: [
+					{ url: '95001.png', legenda: 'Vereador' }
+				]
+			},
+			{
+				numero: '99001',
+				nome: 'Darth Maul',
+				partido: 'PVL',
+				foto: [
+					{ url: '99001.png', legenda: 'Vereador' }
+				]
+			},
+		]
+	},
+	{
+		titulo: 'PREFEITO',
+		numeros: 2,
+		candidatos: [
+			{
+				numero: '84',
+				nome: 'Leia Skywalker',
+				partido: 'PRE',
+				vice: 'Mulher Maravilha',
+				foto: [
+					{ url: '84.png', legenda: 'Prefeito' },
+					{ url: '84_2.png', legenda: 'Vice-Prefeito', small: true }
+				]
+			},
+			{
+				numero: '88',
+				nome: 'Batman',
+				partido: 'PJM',
+				vice: 'Robin',
+				foto: [
+					{ url: '88.png', legenda: 'Prefeito' },
+					{ url: '88_2.png', legenda: 'Vice-Prefeito', small: true }
+				]
+			},
+			{
+				numero: '95',
+				nome: 'Capitão América',
+				partido: 'PLJ',
+				vice: 'Homem de Ferro',
+				foto: [
+					{ url: '95.png', legenda: 'Prefeito' },
+					{ url: '95_2.png', legenda: 'Vice-Prefeito', small: true }
+				]
+			},
+			{
+				numero: '99',
+				nome: 'Coringa',
+				partido: 'PVL',
+				vice: 'Darth Vader',
+				foto: [
+					{ url: '99.png', legenda: 'Prefeito' },
+					{ url: '99_2.png', legenda: 'Vice-Prefeito', small: true }
+				]
+			}
+		]
+	}
+]
